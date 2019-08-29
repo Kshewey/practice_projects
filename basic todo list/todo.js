62aa71620469b62655351e8bd4950f1615f44e43
Kevin Shewey <Kevin.Shewey@gmail.com>
2019-08-28 21:43:21 -0700

    Simple HTML and added Javascript event listener

---
+++ basic todo list/todo.js
@@ -0,0 +1,10 @@
+// Each item should look like this
+// <li><input type="checkbox" />Write this tutorial</li>
+
+function addNewItem() {
+
+}
+
+
+const btnNew = document.getElementById("btnAdd");
+btnNew.onclick = addNewItem;
